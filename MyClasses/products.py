c81a0bb3157f199520174ddcf18ab5587450dbe2
kevham89 <kevin.hammarstedt@gmail.com>
2026-06-08 15:01:16 +0200

    produkter

--- MyClasses/products.py
+++ MyClasses/products.py
@@ -1,2 +1,15 @@
+# Huvudklass för alla products 
 class products:
-    pass
+    def __init__(self, category, maker, price, inventory, expiry_date, strenght, dosage):
+        self.category = category
+        self.maker = maker
+        self.price = price
+        self.inventory = inventory
+        self.expiry_date = expiry_date
+        self.stryka = strenght
+        self.dosage = dosage
+
+class prescription(products):
+    def __init__(self, category, maker, price, inventory, expiry_date, strenght, dosage):
+        super().__init__(category, maker, price, inventory, expiry_date, strenght, dosage)
+        self.prescription = prescription
